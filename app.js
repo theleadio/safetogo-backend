2cb31171a5476fe0a85f6be7acc9f729b0aa4af7
Edmund Hee <edmund.hee05@gmail.com>
2020-03-28 18:46:49 +0800

    Updated with Borneo Data

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mapRouter = require('./routes/map');
 var searchRouter = require('./routes/search');
+var scrapRouter = require('./routes/scrap/borneo.js');
 
 var app = express();
 app.use(cors());
@@ -32,6 +33,7 @@ app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/map', mapRouter);
 app.use('/search', searchRouter);
+app.use('/scrap', scrapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
